4fe850732ad38d7c88ef6ab2d8a680ce1596bd76
Kelly turner <klynnturner@gmail.com>
2017-12-04 20:16:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const app = "I don't do much."
 
-array kittens = ["Milo", "Otis", "Garfield"]
+var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(Ralph) {
   kittens.push("Ralph")
 }
-array kittens = ["Milo", "Otis", "Garfield"]
+var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivlyPrependKitten(Ralph) {
   kittens.unshift("Ralph")
 }
-array kittens = ["Milo", "Otis", "Garfield"]
+var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyRemoveLastKitten() {
   kittens.slice(3)
 }

1c922e2983278bb59745ef93dde2de06a53651fa
Kelly turner <klynnturner@gmail.com>
2017-12-04 19:50:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much."
 function destructivelyAppendKitten() {
-  destructivelyAppendKitten(name).push("kitten")
+  destructivelyAppendKitten.push("kitten")
   return destructivelyAppendKitten
 }

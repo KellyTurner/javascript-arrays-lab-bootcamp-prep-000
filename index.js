965e161599098974ec95f5392fd7faa0af60b85c
Kelly turner <klynnturner@gmail.com>
2017-12-04 20:00:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(name) {
-  kittens.push("name")
+function destructivelyAppendKitten(Ralph) {
+  kittens.push("Ralph")
 }

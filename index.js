ec96a175de0c70695a13b6ec428c6edd4d8fbde0
Kelly turner <klynnturner@gmail.com>
2017-12-04 20:06:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(Ralph) {
   kittens.push("Ralph")
 }
+var kittens = ["Milo", "Otis", "Garfield"]
+
 function destructivlyPrependKitten(Ralph) {
   kittens.unshift("Ralph")
 }
